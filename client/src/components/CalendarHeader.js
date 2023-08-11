476d241518a8b2f0b2d3fc3c17bb61dab51478cd
Marie Lamoureux <lamoureuxmarie@outlook.com>
2023-08-11 11:17:48 +0200

    Added buttons container and some styles

--- client/src/components/CalendarHeader.js
+++ client/src/components/CalendarHeader.js
@@ -4,6 +4,10 @@ const CalendarHeader = ({ calendarName }) => {
   return (
     <div className="CalendarHeader">
       <h1 className="text-center">{calendarName}</h1>
+      <div className="buttons-container d-flex justify-content-center align-items-center py-3">
+        <button className="btn btn-dark mx-1">Create New Event</button>
+        <button className="btn btn-outline-dark mx-1">Sign out</button>
+      </div>
     </div>
   );
 }

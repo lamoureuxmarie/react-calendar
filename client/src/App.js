8a9a501004880798a677991581aa212ddda17ba9
Marie Lamoureux <lamoureuxmarie@outlook.com>
2023-08-11 11:25:27 +0200

    installed full calendar plugin

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,36 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
-import CalendarHeader from './components/CalendarHeader'
+import CalendarHeader from './components/CalendarHeader';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+const events = [
+  { title: 'Meeting', start: new Date() }
+]
 
 const App = () => {
   return (
     <div className="App">
       <CalendarHeader calendarName={'Bday calendar 🎉'} />
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView='dayGridMonth'
+        weekends={false}
+        events={events}
+        eventContent={renderEventContent}
+      />
     </div>
   );
+}
+
+// a custom render function
+function renderEventContent(eventInfo) {
+  return (
+    <>
+      <b>{eventInfo.timeText}</b>
+      <i>{eventInfo.event.title}</i>
+    </>
+  )
 }
 
 export default App;
